7a7a8371d1f56717858389c37476d618de3e842d
kirillnazarov <nazarovkera@gmail.com>
2017-11-20 22:05:35 +0300

    math actions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,5 +3,14 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+
+    System.out.println(2+2);
+    System.out.println(2*2);
+    System.out.println(2 / 2);
+    System.out.println(2 - 2);
+
+    System.out.println(1/2);
+    System.out.println(1.0 / 2);
+
   }
 }
