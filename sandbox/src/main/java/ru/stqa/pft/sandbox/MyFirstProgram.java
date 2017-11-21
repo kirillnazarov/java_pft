f793d831c8480ceba4d3e452b05619a6b2762c3d
kirillnazarov <nazarovkera@gmail.com>
2017-11-21 14:28:26 +0300

    discard changes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,14 +3,5 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-
-    System.out.println(2+2);
-    System.out.println(2*2);
-    System.out.println(2 / 2);
-    System.out.println(2 - 2);
-
-    System.out.println(1/2);
-    System.out.println(1.0 / 2);
-
   }
 }
